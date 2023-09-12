56ad721c6222847c66033e84e60798d2f33fb66e
AnaRocher <anaisabel81@gmail.com>
2023-09-12 15:06:51 +0200

    First commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+
+
+class TodoController extends Controller
+{
+    public function index() {
+        $todos = Todo::uncompleted()->get();
+        $completedTodos = Todo::completed()->get();
+
+        return view('todos.index', [
+            'todos' => $todos,
+            'completedTodos' => $completedTodos
+        ]);
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'content' => 'required'
+        ]);
+
+        $todo = new Todo();
+        $todo->content = $request->content;
+        $todo->user_id = Auth::id();
+        $todo->save();
+
+        return redirect()->back();
+    }
+
+    public function toggle(Todo $todo) {
+        if (!Gate::allows('update-todos', $todo)){
+            abort(403);
+        }
+
+        $todo->completed = !$todo->completed;
+        $todo->save();
+
+        return redirect()->back();
+    }
+
+    public function destroy(Todo $todo) {
+        if (!Gate::allows('update-todos', $todo)){
+            abort(403);
+        }
+
+        $todo->delete();
+        return redirect()->back();
+    }
+}
